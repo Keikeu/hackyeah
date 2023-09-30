96ce643361c5eaa29a770a33b38dc8c404a2fcdb
jkowalski <jkowalski@apptio.com>
2023-09-30 20:14:05 +0200

    map first impl

--- src/App/UniList/Map.js
+++ src/App/UniList/Map.js
@@ -2,14 +2,32 @@ import React from "react";
 import T from "prop-types";
 import styled from "styled-components/macro";
 
+import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet';
+
 const Box = styled.div`
   width: 40%;
+  height: 100%;
   flex-shrink: 0;
   background-color: var(--neutral-190);
 `;
 
 function Map({ className }) {
-  return <Box className={className}>Mapa{/* KLOPRON TUTAJ */}</Box>;
+  const position = [51.505, -0.09];
+
+  return (<Box className={className}>
+    <MapContainer  center={position} zoom={12} scrollWheelZoom={false}>
+      <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+
+  </Box>);
 }
 
 Map.propTypes = {
